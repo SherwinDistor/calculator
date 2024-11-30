6da149bf2362421b5e1ab225b3e814df2a82f6be
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-30 13:29:54 -0600

    Create functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(3, 4));
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtract(9, 6));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(5, 5));
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(9, 3));
